80caa428ed64c7e91a5c06215074fb7c893833d0
Guillaume <gui.simon@gmail.com>
2023-10-12 12:40:57 +0200

    fix unzip

--- script/unzip.bash
+++ script/unzip.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 test ! -f data.zip && (echo "ERROR: can not find file data.zip. Are you starting this script at the root of the directory ?" && exit 1)
-unzip -n data.zip -d data
+unzip -n data.zip
